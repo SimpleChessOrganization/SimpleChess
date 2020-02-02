7a45f5e02e4df9a59ba82c161f27e570557dc345
SherlockKA <andrey.kurzyukov@siemens-healthineers.com>
2020-02-02 14:18:02 +0100

    remove ServerSettings class and implement run_server()

--- server/main.py
+++ server/main.py
@@ -1,15 +1,22 @@
-import numpy 
-class ServerSettings(obj):
-    def __init__(port):
-        self.port = port
+import http.server
+import socketserver
+from server_settings import ServerSettings
 
 def default_settings():
     return ServerSettings(26777)
 
 def run_server(settings):
-    pass
+    port = settings.port
+    handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", port), handler) as httpd:
+        print("serving at port", port)
+        httpd.serve_forever()
 
 def main():
     settings = default_settings()
     run_server(settings)
+
+if __name__ == "__main__":
+    main()
 
