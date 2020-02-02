e125cb2be114ae8f6501565315fdaaffde2ce14d
Mikhail Amirov <amirovm@amirovm-macbookair.roam.corp.google.com>
2020-02-02 11:22:42 +0100

    Created server requirements and main

---
+++ server/main.py
@@ -0,0 +1,15 @@
+
+class ServerSettings(obj):
+    def __init__(port):
+        self.port = port
+
+def default_settings():
+    return ServerSettings(26777)
+
+def run_server(settings):
+    pass
+
+def main():
+    settings = default_settings()
+    run_server(settings)
+
